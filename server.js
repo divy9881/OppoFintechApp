fec174c7d84789628392b5af031cc29bebd21115
DivyPatel9881 <divy9881@gmail.com>
2019-12-10 15:57:59 +0530

    env var added

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("/", function(req, res){
     res.send("Oppo fintech app running ....")
 })
 
-server = app.listen(port, function(){
+server = app.listen(process.env.PORT, function(){
     console.log("Server listening at " + server.address().address + " and on " + server.address().port)
 })
