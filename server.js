cbe7f8fc6a227cf66b35c1084df57508ab2ebf7e
DivyPatel9881 <divy9881@gmail.com>
2019-12-13 01:42:20 +0530

    Small twaeks

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", function(req, res){
 })
 
 app.post("/", function(req, res){
-    console.log("Body is " + JSON.stringify(req.body) )
+    console.log(req.body)
 })
 
 let server = app.listen(process.env.PORT, function(){
