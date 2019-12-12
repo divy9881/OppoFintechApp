f8234145d4391619514ff2362383ea9ff0e8d6d6
DivyPatel9881 <divy9881@gmail.com>
2019-12-12 22:24:53 +0530

    Body-parser added

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 let express = require("express")
+let bodyParser = require("body-parser")
 
 let app = express()
 
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.get("/", function(req, res){
+    console.log("GET request accepted.")
     res.send("Oppo fintech app running ....")
 })
 
-server = app.listen(process.env.PORT, function(){
+app.post("/", function(req, res){
+    console.log(req.body)
+})
+
+let server = app.listen(process.env.PORT, function(){
     console.log("Server listening at " + server.address().address + " and on " + server.address().port)
 })
