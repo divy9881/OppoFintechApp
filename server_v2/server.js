acc692589ffdeb9bcf8b8236a6f0c5aba552034b
DivyPatel9881 <divy9881@gmail.com>
2020-01-01 19:23:38 +0530

    Deploying v2 app.

--- server_v2/server.js
+++ server_v2/server.js
@@ -7,7 +7,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // app.use(express.static(__dirname + "/static"))
-
+app.get("/", function(req, res){
+    res.send("V2 app running ...")
+})
 
 let server = app.listen(process.env.PORT, function () {
     console.log("Server listening on " + server.address().port)
